c6d1269aa54d3df0f7588228668d849e92c52961
hiphapis <hiphapis@gmail.com>
2009-07-17 14:40:24 +0000

    Add link_to_tag(s) To Helper

--- lib/tags_helper.rb
+++ lib/tags_helper.rb
@@ -1,13 +1,23 @@
 module TagsHelper
-  # See the README for an example using tag_cloud.
-  def tag_cloud(tags, classes)
-    return if tags.empty?
-    
-    max_count = tags.sort_by(&:count).last.count.to_f
-    
-    tags.each do |tag|
-      index = ((tag.count / max_count) * (classes.size - 1)).round
-      yield tag, classes[index]
-    end
-  end
+	# See the README for an example using tag_cloud.
+	def tag_cloud(tags, classes)
+		return if tags.empty?
+		
+		max_count = tags.sort_by(&:count).last.count.to_f
+		
+		tags.each do |tag|
+			index = ((tag.count / max_count) * (classes.size - 1)).round
+			yield tag, classes[index]
+		end
+	end
+
+
+	def link_to_tag(tag, options = {})
+		link_to tag, { :action => :tag, :id => tag }, options
+	end
+
+	def link_to_tags(tags, options = {})
+		delimiter = options[:delimiter] || ", "
+		tags.collect { |tag| link_to_tag(tag.name, options) }.join(delimiter)
+	end
 end
--- test/tags_helper_test.rb
+++ test/tags_helper_test.rb
@@ -1,28 +1,42 @@
 require File.dirname(__FILE__) + '/abstract_unit'
 
 class TagsHelperTest < Test::Unit::TestCase
-  fixtures :tags, :taggings, :posts
-  
-  include TagsHelper
-  
-  def test_tag_cloud
-    cloud_elements = []
-    
-    tag_cloud Post.tag_counts, %w(css1 css2 css3 css4) do |tag, css_class|
-      cloud_elements << [tag, css_class]
-    end
-    
-    assert_equal [
-      [tags(:good), "css2"],
-      [tags(:bad), "css1"],
-      [tags(:nature), "css4"],
-      [tags(:question), "css1"]
-    ], cloud_elements
-  end
-  
-  def test_tag_cloud_when_no_tags
-    tag_cloud SpecialPost.tag_counts, %w(css1) do
-      assert false, "tag_cloud should not yield"
-    end
-  end
+	fixtures :tags, :taggings, :posts
+	
+	include TagsHelper
+	
+	def test_tag_cloud
+		cloud_elements = []
+		
+		tag_cloud Post.tag_counts, %w(css1 css2 css3 css4) do |tag, css_class|
+			cloud_elements << [tag, css_class]
+		end
+		
+		assert_equal [
+			[tags(:good), "css2"],
+			[tags(:bad), "css1"],
+			[tags(:nature), "css4"],
+			[tags(:question), "css1"]
+		], cloud_elements
+	end
+	
+	def test_tag_cloud_when_no_tags
+		tag_cloud SpecialPost.tag_counts, %w(css1) do
+			assert false, "tag_cloud should not yield"
+		end
+	end
+
+	#
+	# link_to_*
+	#
+	def test_link_to_tag
+		tag_name = "JustTest!"
+		assert_equal link_to(tag_name, { :action => :tag, :id => tag_name }), link_to_tag(tag_name)
+	end
+
+	def test_link_to_tags
+		tag_links = link_to_tags(Post.first)
+
+		assert_equal "#{link_to_tag(Post.first.tags.first.name)}, #{link_to_tag(Post.first.tags.last.name)}", tag_links
+	end
 end
